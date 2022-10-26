9a6ddcb686cdcd264309d26e21f1819bce6203c8
harshachaitanya27 <harshachaitanya27@gmail.com>
2022-10-26 16:58:03 -0700

    Updated the integration test

--- MyLibrary/Sources/MyLibrary/mockserver.py
+++ MyLibrary/Sources/MyLibrary/mockserver.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import jsonify
 import requests
 import pprint
 import json
@@ -12,11 +11,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return json.dumps(data,indent=2)
+    return json.dumps(data)
  
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(debug=True,port=3000)
+    app.run(debug=True)
